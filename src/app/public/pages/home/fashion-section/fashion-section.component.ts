9de97282df838d2f6e1ffe52e475de1119d1f5c5
vaso-max <vasiko.gagloshvili@mail.ru>
2021-07-14 19:08:50 +0400

    fashion-section

---
+++ src/app/public/pages/home/fashion-section/fashion-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fashion-section',
+  templateUrl: './fashion-section.component.html',
+  styleUrls: ['./fashion-section.component.css']
+})
+export class FashionSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
