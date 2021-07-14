686a549226e48534d62af72a46e0f2381e9f10ad
vaso-max <vasiko.gagloshvili@mail.ru>
2021-07-14 18:19:49 +0400

    main carusel is ready | home's component

---
+++ src/app/public/pages/home/main-carusel/main-carusel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-carusel',
+  templateUrl: './main-carusel.component.html',
+  styleUrls: ['./main-carusel.component.css']
+})
+export class MainCaruselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
