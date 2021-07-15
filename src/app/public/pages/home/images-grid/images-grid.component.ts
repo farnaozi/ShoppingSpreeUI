3f2b9b660297eb2543fb08dfb00a0ccc7fb882bc
vaso-max <vasiko.gagloshvili@mail.ru>
2021-07-15 20:15:12 +0400

    home component progressing

---
+++ src/app/public/pages/home/images-grid/images-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-images-grid',
+  templateUrl: './images-grid.component.html',
+  styleUrls: ['./images-grid.component.css']
+})
+export class ImagesGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
