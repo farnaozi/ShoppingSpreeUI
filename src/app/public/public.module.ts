44b1df3bc66e66de80b5084509f9da26a1fc0807
farnaozi <sarishvilifarna@gmail.com>
2021-07-17 15:58:09 +0400

    checkout page 50% done

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -11,9 +11,12 @@ import { ProductsModule } from './pages/products/products.module';
 import { ShoppingCartModule } from './pages/shopping-cart/shopping-cart.module';
 import { BreadcrumbComponent } from '../shared/components/breadcrumb/breadcrumb.component';
 import { PublicComponent } from './public.component';
-import { CarouselModule } from 'ngx-owl-carousel-o';
 import { HotSalesComponent } from './pages/home/hot-sales/hot-sales.component';
 import { TopBrandsComponent } from './pages/home/top-brands/top-brands.component';
+
+import { CheckoutModule } from './pages/checkout/checkout.module';
+import { SheredComponentsModule } from '../shared/modules/shered-components.module';
+
 
 
 
@@ -35,7 +38,9 @@ import { TopBrandsComponent } from './pages/home/top-brands/top-brands.component
   imports: [
     CommonModule,
     ProductsModule,
-    ShoppingCartModule
+    ShoppingCartModule,
+    CheckoutModule,
+    SheredComponentsModule
   ],
   exports: [
     PublicComponent
