153314e8561b57cdd2d37ee9119e1afe1d7cb0f5
farnaozi <sarishvilifarna@gmail.com>
2021-07-19 18:26:27 +0400

    product page 80% done

---
+++ src/app/public/pages/product/buying-details-section/buying-details-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buying-details-section',
+  templateUrl: './buying-details-section.component.html',
+  styleUrls: ['./buying-details-section.component.css']
+})
+export class BuyingDetailsSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/public/pages/product/description-review-section/description-review-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-description-review-section',
+  templateUrl: './description-review-section.component.html',
+  styleUrls: ['./description-review-section.component.css']
+})
+export class DescriptionReviewSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
