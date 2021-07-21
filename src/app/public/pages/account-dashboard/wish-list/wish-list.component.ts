675771de6d97743fae7022564cd735ea6ba0afae
vaso-max <vasiko.gagloshvili@mail.ru>
2021-07-21 14:10:03 +0400

    wishlist and order in account info page

---
+++ src/app/public/pages/account-dashboard/wish-list/wish-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-list',
+  templateUrl: './wish-list.component.html',
+  styleUrls: ['./wish-list.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class WishListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
